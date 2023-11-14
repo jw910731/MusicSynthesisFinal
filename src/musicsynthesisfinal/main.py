737a9354dd683648f177598e7b06ad21eece0eeb
jw910731 <jw910731@gmail.com>
2023-11-15 00:18:39 +0800

    Add auto inference device selector

--- src/musicsynthesisfinal/main.py
+++ src/musicsynthesisfinal/main.py
@@ -1,10 +1,23 @@
 from ai_note_factory import AiNoteFactory
 from music21.stream import Stream
+import torch
+
+
+def device_chooser():
+    if torch.backends.mps.is_built():
+        return "mps"
+    if torch.backends.cudnn.is_available():
+        return "cudnn"
+    if torch.cuda.is_available():
+        return "cuda"
+    return "cpu"
 
 
 def main():
+    dev_str = device_chooser()
+    print("Currently using \"{}\" for inference.".format(dev_str))
     factory = AiNoteFactory(
-        "a uk drill mix boombap.", device="cpu")
+        "This is a traditional Irish dance music. In G major", device=dev_str)
     stream = Stream()
     part = factory.get_notes()
     if part.isWellFormedNotation():
