7c88d24e60ae1d65652e795bb12bbf0ec4125235
七詩パンダ <danal99151@gmail.com>
2023-12-01 22:30:31 +0800

    classical

---
+++ src/musicsynthesisfinal/classical.py
@@ -0,0 +1,17 @@
+import beat, chord, melody
+        
+class ClassicalBeat(beat.Beat):
+    def generate_beat(self)->list[music21.duration.Duration]:
+        pass
+class ClassicalChord(chord.Chord):
+    def generate_chord(self)->music21.chord.Chord:
+        pass
+class ClassicalMelody(melody.Melody):
+    def generate_melody(self)->music21.stream.Measure:
+        pass
+class Classical:
+    def __init__(self):
+        self.melody = ClassicalMelody
+        self.chord = ClassicalChord
+        self.beat = ClassicalBeat
+    
