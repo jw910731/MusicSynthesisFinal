72abd3c82bada1618366dc729f18195aea99ee2f
jw910731 <jw910731@gmail.com>
2023-12-03 04:19:42 +0800

    Refactor code for obey PEP8

--- src/musicsynthesisfinal/classical.py
+++ src/musicsynthesisfinal/classical.py
@@ -1,7 +1,12 @@
-import beat, chord, melody
-import music21
 import random
+
+import music21
+
+import beat
+import chord
 import data
+import melody
+
 
 class ClassicalBeat(beat.Beat):
     PROBABILITY = {'Allegro': [0.02, 0.12, 0.45, 0.30, 0.09, 0.02],
@@ -16,11 +21,6 @@ class ClassicalBeat(beat.Beat):
         ClassicalBeat class representing a classical beat.
 
         This class inherits from the Beat class.
-
-        Attributes:
-            speed_type (str): The type of speed for the beat.
-            prefix (list of int): The prefix values for the beat probabilities.
-            bpm (int): The beats per minute for the beat.
         """
         self.speed_type, self.durationProbability = random.choice(list(self.PROBABILITY.items()))
         self.bpm = random.randint(*self.BPMRANGE[self.speed_type])
@@ -32,9 +32,9 @@ class ClassicalBeat(beat.Beat):
         Returns:
             float: The duration of the beat in quarter note units.
         """
-        
-        r = data.weightRandomValuable(self.durationProbability)
-        return 2 ** (r-3)
+
+        r = data.weight_random_valuable(self.durationProbability)
+        return 2 ** (r - 3)
 
     def generate_beat(self, n) -> list[music21.duration.Duration]:
         """
@@ -49,7 +49,7 @@ class ClassicalBeat(beat.Beat):
         now = 0
         ret = []
         while now < n:
-            choice_beat = self.__get_random_beat();
+            choice_beat = self.__get_random_beat()
             if now + choice_beat > n:
                 choice_beat = n - now
             ret.append(music21.duration.Duration(choice_beat))
@@ -71,29 +71,31 @@ class ClassicalBeat(beat.Beat):
 
 class ClassicalChord(chord.Chord):
     def __init__(self, tone):
-        self.__Auto__ = {'I'  :{'IV': 0.5, 'vi': 0.5}, 
-                         'ii' :{'IV': 0.5, 'V': 0.5},
-                         'iii':{'ii': 0.5, 'IV': 0.5},
-                         'IV' :{'ii': 0.25, 'iii': 0.25, 'V': 0.25, 'vi': 0.2, 'I': 0.05},
-                         'V'  :{'iii':0.3, 'vi': 0.5,'I':0.2},
-                         'vi' :{'iii':0.3, 'IV': 0.3,'V':0.4}
-                        }
+        self.__Auto__ = {'I': {'IV': 0.5, 'vi': 0.5},
+                         'ii': {'IV': 0.5, 'V': 0.5},
+                         'iii': {'ii': 0.5, 'IV': 0.5},
+                         'IV': {'ii': 0.25, 'iii': 0.25, 'V': 0.25, 'vi': 0.2, 'I': 0.05},
+                         'V': {'iii': 0.3, 'vi': 0.5, 'I': 0.2},
+                         'vi': {'iii': 0.3, 'IV': 0.3, 'V': 0.4}
+                         }
         self.nowChord = 'I'
         self.lastChord = 'ST'
         self.tone = tone
+
     def generate_chord(self) -> music21.chord.Chord:
         return music21.roman.RomanNumeral(self.generate_roman(), self.tone)
-    def __nextChord(self, nowLoc) -> None:
-        val, pro = list(self.__Auto__[nowLoc].keys()),\
-                   list(self.__Auto__[nowLoc].values())
-        choose = data.weightRandomValuable(pro)
-        while (val[choose] == self.lastChord) :
-            choose = data.weightRandomValuable(pro)
+
+    def __next_chord(self, now_location) -> None:
+        val, pro = list(self.__Auto__[now_location].keys()), \
+            list(self.__Auto__[now_location].values())
+        choose = data.weight_random_valuable(pro)
+        while val[choose] == self.lastChord:
+            choose = data.weight_random_valuable(pro)
         self.lastChord, self.nowChord = self.nowChord, val[choose]
-        
+
     def generate_roman(self) -> str:
         ret = self.nowChord
-        self.__nextChord(self.nowChord)
+        self.__next_chord(self.nowChord)
         return ret
 
 
@@ -101,9 +103,10 @@ class ClassicalMelody(melody.Melody):
     def __init__(self) -> None:
         # super().__init__()
         pass
+
     def generate_melody(self) -> music21.stream.Measure:
         # how to pass the chord and beat?
-        
+
         pass
 
 
--- src/musicsynthesisfinal/data.py
+++ src/musicsynthesisfinal/data.py
@@ -1,4 +1,5 @@
 import random
+
 NOTE_NUMBER_CONV = {
     'C': 12, 'C#': 13, 'D': 14, 'D#': 15, 'E': 16, 'F': 17, 'F#': 18, 'G': 19, 'G#': 20, 'A': 21, 'B-': 22, 'B': 23,
     12: 'C', 13: 'C#', 14: 'D', 15: 'D#', 16: 'E', 17: 'F', 18: 'F#', 19: 'G', 20: 'G#', 21: 'A', 22: 'B-', 23: 'B'
@@ -22,14 +23,16 @@ STANDARD_TONE_NOTES = {'C major': ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
 BASS_SCALE = [35, 36, 41, 43, 45, 47]
 HIHAT_SCALE = [42, 44, 46, 49, 51, 59]
 SNARE_SCALE = [37, 38, 40, 48, 50]
-def weightRandomValuable(probability):
+
+
+def weight_random_valuable(probability):
     prefix = []
     # print(probability)
     for i in probability:
-        prefix.append(i*100)
+        prefix.append(i * 100)
         if len(prefix) > 1:
             prefix[-1] += prefix[-2]
-    r = random.randint(1,100)
+    r = random.randint(1, 100)
     for p in range(len(prefix)):
         if r <= prefix[p]:
             return p
