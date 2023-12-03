fb558079ac7125e2f6d05f8cb4385c43a03f648d
jw910731 <jw910731@gmail.com>
2023-12-03 21:22:51 +0800

    Try a bad approach to generate melody in test script

--- src/musicsynthesisfinal/test.py
+++ src/musicsynthesisfinal/test.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 import classical
 import music21
@@ -16,14 +17,27 @@ for i in range(4):
     sum_beat = 0
     for bt in beat:
         sum_beat += bt.quarterLength
-        p1.append(music21.note.Note(data.NATURAL_SCALE[tone][0], duration=bt))
 
-    ch = music21.stream.Measure()
-    for _ in range(math.ceil(sum_beat/4)):
+    chords = []
+    for _ in range(math.ceil(sum_beat / 4)):
         ch = c.generate_chord()
         ch.quarterLength = 4
         ch = ch.closedPosition(forceOctave=3)
         p2.append(ch)
+        chords.append(ch)
+
+    off_tone = 0  # off tone duration in quarter length
+    measure_acc = 0  # measure accumulator in quarter length
+    for bt in beat:
+        current_measure = math.floor(measure_acc) // 4
+        note = music21.note.Note(random.choice(data.NATURAL_SCALE[tone]), duration=bt)
+        while note.pitch.pitchClass not in (chord.pitchClass for chord in
+                                            chords[current_measure].pitches) and off_tone > 2:
+            note = music21.note.Note(random.choice(data.NATURAL_SCALE[tone]), duration=bt)
+        else:
+            off_tone += bt.quarterLength
+        p1.append(note)
+        measure_acc += bt.quarterLength / 4
 
 s = music21.stream.Stream()
 s.insert(0, p1)
