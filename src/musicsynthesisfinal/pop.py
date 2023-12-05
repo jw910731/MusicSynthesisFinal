65ac90e10083eeaadd15862b083fded919163fe1
jw910731 <jw910731@gmail.com>
2023-12-05 22:47:37 +0800

    Add pop music common chord progression
    Bala song generator!

--- src/musicsynthesisfinal/pop.py
+++ src/musicsynthesisfinal/pop.py
@@ -1,28 +1,50 @@
 import beat, chord, melody
 import music21
-import random   
+import random
+
+
 class PopBeat(beat.Beat):
     def __init__(self):
         pass
+
     def __get_random_beat(self):
         pass
+
     def generate_beat(self, n) -> list[music21.duration.Duration]:
         pass
+
     def get_bpm(self):
         return self.bpm
+
     def generate_bass(self):
         pass
+
     def generate_hihat(self):
         pass
+
     def generate_snare(self):
         pass
 
+
 class PopChord(chord.Chord):
-    def __init__(self) -> None:
-        super().__init__()
-        chordProcessor = ['C', 'G/B', 'Am', 'Em/G', 'F', 'C/E', 'Dm', 'G']
+    AVAILABLE_PROGRESSION = [
+        ["I", "V", "vi", "iii", "IV", "I", "IV", "V"],
+        ["I", "V", "vi", "VI"],
+        ["vi", "VI", "I", "V"],
+        ['IV', "V", "iii", "vi"],
+        ['IV', "V", "iii", "vi", "ii", "V", "I"],
+    ]
+
+    def __init__(self, tone) -> None:
+        self.progressions = random.choice(self.AVAILABLE_PROGRESSION)
+        self.pivot = 0
+        self.tone = tone
+
     def generate_chord(self) -> music21.chord.Chord:
-        pass
+        chrd = music21.roman.RomanNumeral(self.progressions[self.pivot], self.tone)
+        chrd.semiClosedPosition(forceOctave=3, inPlace=True)
+        self.pivot = (self.pivot + 1) % len(self.progressions)
+        return chrd
 
 
 class PopMelody(melody.Melody):
