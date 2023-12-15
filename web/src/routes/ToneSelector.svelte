b6dd063cb54bb723b9108a6951a81d124ef6d8a0
jw910731 <jw910731@gmail.com>
2023-12-15 22:55:48 +0800

    Change ToneSelector grid split

--- web/src/routes/ToneSelector.svelte
+++ web/src/routes/ToneSelector.svelte
@@ -32,25 +32,25 @@
     })();
 </script>
 <div>
-    <div class="grid grid-cols-12 gap-4">
-        <div class="select col-span-2">
-            <ListBox size="4">
+    <div class="grid grid-cols-3 gap-4">
+        <div class="select col-span-1">
+            <ListBox>
                 <ListBoxItem bind:group={selectedNote} name="note" value="random">Random</ListBoxItem>
                 {#each notes as note}
                     <ListBoxItem bind:group={selectedNote} name="note" value={note}>{note}</ListBoxItem>
                 {/each}
             </ListBox>
         </div>
-        <div class="select col-span-2">
-            <ListBox size="4">
+        <div class="select col-span-1">
+            <ListBox>
                 <ListBoxItem bind:group={selectedAccidental} name="accidental" value="random">Random</ListBoxItem>
                 {#each accidentals as [val, display]}
                     <ListBoxItem bind:group={selectedAccidental} name="accidental" value={val}>{display}</ListBoxItem>
                 {/each}
             </ListBox>
         </div>
-        <div class="select col-span-2">
-            <ListBox size="1">
+        <div class="select col-span-1">
+            <ListBox>
                 {#each types as [val, display]}
                     <ListBoxItem bind:group={selectedType} name="type" value={val}>{display}</ListBoxItem>
                 {/each}
