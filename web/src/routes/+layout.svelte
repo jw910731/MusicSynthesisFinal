efe9056436ae56d087221a8b66e30128108d88ca
jw910731 <jw910731@gmail.com>
2023-12-15 17:33:46 +0800

    Fix UI template

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,1 +1,30 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+    import "../app.postcss";
+    import { autoModeWatcher, AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+
+</script>
+
+<svelte:head>
+    {@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
+</svelte:head>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<h3 class="h3 mr-auto"> Music and Sound Synthesis Final Project </h3>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
