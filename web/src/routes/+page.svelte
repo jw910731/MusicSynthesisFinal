10fca9aa93a1f5e3bf9e5fcbcc9206739f59e939
jw910731 <jw910731@gmail.com>
2023-12-15 22:55:28 +0800

    Add StyleSelector

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <script>
     import ToneSelector from "./ToneSelector.svelte";
+    import StyleSelector from "./StyleSelector.svelte";
 
     let tone = "";
+    let style = "";
 </script>
 
 <div class="container mx-auto">
-    <div>
-        <p class="my-2">Tone Selection: </p>
-        <ToneSelector bind:tone />
-        <p>Tone: {tone}</p>
+    <div class="grid grid-cols-2 gap-2">
+        <div>
+            <p class="my-2">Tone Selection: </p>
+            <ToneSelector bind:tone/>
+            <p>Tone: {tone}</p>
+        </div>
+        <div>
+            <p class="my-2">Style: </p>
+            <StyleSelector bind:style/>
+            <p>Style: {style}</p>
+        </div>
     </div>
 </div>
--- web/src/routes/StyleSelector.svelte
+++ web/src/routes/StyleSelector.svelte
@@ -1,7 +1,51 @@
-<script>
+<script lang="ts">
+    import {ListBox, ListBoxItem} from "@skeletonlabs/skeleton";
+
+    type Style = {
+        display: string,
+        variant: string[],
+    };
+    const styles: Record<string, Style> = {
+        "classical": {
+            display: "Classical",
+            variant: ["Allegro", "Moderato", "Andante"],
+        },
+        "pop": {
+            display: "Pop",
+            variant: [],
+        },
+        "hiphop": {
+            display: "Hiphop",
+            variant: ["Boombap", "Drill", "Trap"],
+        }
+    };
+    let selectedStyle = "";
+    let selectedVar = "";
+
+    $: if(!!styles[selectedStyle] && Array.isArray(styles[selectedStyle].variant) && styles[selectedStyle].variant.length <= 0) selectedVar = ""
+
+    export let style = "";
+    $: style = selectedStyle + ((!!selectedVar) ? ("::" + selectedVar) : "");
 
 </script>
 
 <div>
-
+    <div class="grid grid-cols-2 gap-4">
+        <div class="select">
+            <ListBox>
+                {#each Object.entries(styles) as [key, style]}
+                    <ListBoxItem bind:group={selectedStyle} name="style" value={key}>{style.display}</ListBoxItem>
+                {/each}
+            </ListBox>
+        </div>
+        <div class="select">
+            {#if !!styles[selectedStyle] && Array.isArray(styles[selectedStyle].variant) && styles[selectedStyle].variant.length}
+                <ListBox>
+                    {#each styles[selectedStyle].variant as variant}
+                        <ListBoxItem bind:group={selectedVar} name="style" value={variant}>{variant}</ListBoxItem>
+                    {/each}
+                </ListBox>
+            {/if}
+        </div>
+    </div>
 </div>
